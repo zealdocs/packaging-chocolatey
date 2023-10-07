1be1140538d1f9ef22ad2bca3047ec742d1d88c6
Oleg Shparber <trollixx@gmail.com>
2023-10-07 00:48:32 -0400

    chore: add missing update for v0.6.1

--- zeal.install/tools/chocolateyinstall.ps1
+++ zeal.install/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageArgs = @{
   packageName    = 'zeal.install'
   fileType       = 'msi'
-  url            = 'https://github.com/zealdocs/zeal/releases/download/v0.5.0/zeal-0.5.0-windows-x86.msi'
-  checksum       = 'edb710ac977762726365f934f5fadb327c504f1ec4d7bbf71c1b84a52d6cbd18'
+  url            = 'https://github.com/zealdocs/zeal/releases/download/v0.6.1/zeal-0.6.1-windows-x86.msi'
+  checksum       = 'f4796664b64d30317cf50914f0ff730139bb0b40656aaba5376769976896025a'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.5.0/zeal-0.5.0-windows-x64.msi'
-  checksum64     = 'b73ea868f0abeba3cc5c181e78173204c1de0a1af2c74f18cbe8e097b591b238'
+  url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.6.1/zeal-0.6.1-windows-x64.msi'
+  checksum64     = '875a91b0aa91a7dc56ef23b551006fd64b33ebfb16b710e4924c391751e6d515'
   checksumType64 = 'sha256'
   silentArgs     = '/quiet'
   validExitCodes = @(0,3010)
--- zeal.portable/tools/chocolateyinstall.ps1
+++ zeal.portable/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   PackageName    = 'zeal.portable'
-  Url            = 'https://github.com/zealdocs/zeal/releases/download/v0.5.0/zeal-portable-0.5.0-windows-x86.7z'
-  Checksum       = 'c638a65b7cb9b18efaaee1192ede83cecbc716095a3b28778c31fadb9b01ef3b'
+  Url            = 'https://github.com/zealdocs/zeal/releases/download/v0.6.1/zeal-portable-0.6.1-windows-x86.7z'
+  Checksum       = '9ab3fd0c15101afe3e6be18afe892868c37784d4bf556a81311b3544621501ae'
   ChecksumType   = 'sha256'
-  Url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.5.0/zeal-portable-0.5.0-windows-x64.7z'
-  Checksum64     = '4a517d03825cf300efd9405ff101e890004c0824abea9a7637d661885d9d8f3c'
+  Url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.6.1/zeal-portable-0.6.1-windows-x64.7z'
+  Checksum64     = '08e9992f620ba0a5ea348471d8ac9c85059e95eedd950118928be639746e3f94'
   ChecksumType64 = 'sha256'
   UnzipLocation  = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 }
