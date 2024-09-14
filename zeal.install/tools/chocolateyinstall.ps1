0662b639e5160006e62c0424f9df4d3eda93a8a0
Oleg Shparber <trollixx@gmail.com>
2024-09-14 00:53:17 -0400

    feat: update to Zeal v0.7.2

--- zeal.install/tools/chocolateyinstall.ps1
+++ zeal.install/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0, 3010, 1641)
 
-  url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.7.1/zeal-0.7.1-windows-x64.msi'
-  checksum64     = '088024009bc7c327f9fd8e309d89942781e28a471c2dddef9e20177ccc135dda'
+  url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.7.2/zeal-0.7.2-windows-x64.msi'
+  checksum64     = 'aa41c00c143e2ceb020b080b7a61c9d1bd5e9bb358645a1f8c1ecb5a4af2abc8'
   checksumType64 = 'sha256'
 }
 
--- zeal.portable/tools/chocolateyinstall.ps1
+++ zeal.portable/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   PackageName    = 'zeal.portable'
   UnzipLocation  = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-  Url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.7.1/zeal-0.7.1-portable-windows-x64.7z'
-  Checksum64     = 'a8a03ec9671823d744fd7afe504c8b46146cb1f6b96c6b019c7ac21a3aa523b1'
+  Url64bit       = 'https://github.com/zealdocs/zeal/releases/download/v0.7.2/zeal-0.7.2-portable-windows-x64.7z'
+  Checksum64     = '15d2d1417678a8a035cac58385d540949f8ffc63bceb3379d471a02509e10c32'
   ChecksumType64 = 'sha256'
 }
 
